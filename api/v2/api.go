a531558ffd861ca5f241791719f9f3b77a46a233
dongmen <414110582@qq.com>
2024-07-11 16:38:33 +0800

    fix some error
    Signed-off-by: dongmen <414110582@qq.com>

--- api/v2/api.go
+++ api/v2/api.go
@@ -15,7 +15,7 @@ package v2
 
 import (
 	"github.com/flowbehappy/tigate/api/middleware"
-	appctx "github.com/flowbehappy/tigate/common/context"
+	appctx "github.com/flowbehappy/tigate/pkg/common/context"
 	"github.com/gin-gonic/gin"
 )
 
