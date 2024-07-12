c51f6a2d357352d8e5466aa4543430ebb1dd21d1
flow <flowbehappy@gmail.com>
2024-07-12 21:04:07 +0800

    Refactor task scheduler

--- utils/threadpool/thread_pool_bench_test.go
+++ utils/threadpool/thread_pool_bench_test.go
@@ -27,61 +27,18 @@ func shuffle(slice []int) {
 	}
 }
 
-// BenchmarkCPUThreadPool and BenchmarkCPUThreadPoolWithWaitReactor want to compare the performance of CPU ThreadPool and WaitReactor
-// We make a task, that when the chan fetch the value, then do atomic.Add tasls
-// We implementation the task in two ways (1. as CPU Task 2. as CPU + Wait Task) to compare the cost time and cpu usage.
-func benchmarkCPUThreadPool(addCount int, sleepTime int, b *testing.B) {
-	count := 1000
-	for k := 0; k < b.N; k++ {
-		testCount = 0
-		channel := make(chan int, 1)
-		chanVec := make([]chan int, count)
-		for i := 0; i < count; i++ {
-			chanVec[i] = make(chan int, 1)
-		}
-		taskScheduler := NewTaskScheduler(&DefaultTaskSchedulerConfig, "PrueCPUTask")
-		for i := 0; i < count; i++ {
-			taskScheduler.Submit(newPureCPUTask(&channel, &chanVec[i], int64(count), addCount))
-		}
-
-		index := make([]int, count)
-		for i := 0; i < count; i++ {
-			index[i] = i
-		}
-		shuffle(index)
-
-		for i := 0; i < count; i++ {
-			chanVec[index[i]] <- 1
-			time.Sleep(time.Duration(sleepTime) * time.Microsecond)
-		}
-
-		<-channel
-		taskScheduler.Finish()
-	}
-}
-
 func benchmarkCPUThreadPoolWithWaitReactor(addCount int, sleepTime int, b *testing.B) {
 	count := 1000
+	delay := time.Duration(sleepTime) * time.Microsecond
 	for k := 0; k < b.N; k++ {
 		testCount = 0
 		channel := make(chan int, 1)
-		chanVec := make([]chan int, count)
-		for i := 0; i < count; i++ {
-			chanVec[i] = make(chan int, 1)
-		}
-		taskScheduler := NewTaskScheduler(&DefaultTaskSchedulerConfig, "CPUWithWaitTask")
-		for i := 0; i < count; i++ {
-			taskScheduler.Submit(newCPUWithWaitTask(&channel, &chanVec[i], int64(count), addCount))
-		}
-		index := make([]int, count)
-		for i := 0; i < count; i++ {
-			index[i] = i
-		}
-		shuffle(index)
 
+		taskScheduler := NewTaskSchedulerDefault("CPUWithWaitTask")
 		for i := 0; i < count; i++ {
-			chanVec[index[i]] <- 1
-			time.Sleep(time.Duration(sleepTime) * time.Microsecond)
+			// We make the scheduler to call the Execute immediately.
+			// And the Execute will return the next execution time, i.e. time.Now() + delay
+			taskScheduler.Submit(newCPUWithWaitTask(&channel, count, addCount, delay), CPUTask, time.Now())
 		}
 
 		<-channel
@@ -102,27 +59,14 @@ func BenchmarkCPUThreadPoolWithWaitReactor10000x1000(b *testing.B) {
 	benchmarkCPUThreadPoolWithWaitReactor(10000, 1000, b)
 }
 
-func BenchmarkCPUThreadPool10000x1(b *testing.B) {
-	benchmarkCPUThreadPool(10000, 1, b)
-}
-func BenchmarkCPUThreadPool10000x10(b *testing.B) {
-	benchmarkCPUThreadPool(10000, 10, b)
-}
-func BenchmarkCPUThreadPool10000x100(b *testing.B) {
-	benchmarkCPUThreadPool(10000, 100, b)
-}
-func BenchmarkCPUThreadPool10000x1000(b *testing.B) {
-	benchmarkCPUThreadPool(10000, 1000, b)
-}
-
 // These two test cases are used to compare the cost time and cpu usage of ThreadPool and GoRoutine.
 func benchmarkCostThreadPool(taskCount int, addCount int, b *testing.B) {
-	taskScheduler := NewTaskScheduler(&DefaultTaskSchedulerConfig, "CPUTimeTask")
+	taskScheduler := NewTaskScheduler("CPUTimeTask", 0, -1)
 	for k := 0; k < b.N; k++ {
 		testCount = 0
 		channel := make(chan int, 1)
 		for i := 0; i < taskCount; i++ {
-			taskScheduler.Submit(newCPUTimeTask(&channel, addCount, taskCount))
+			taskScheduler.Submit(newCPUTimeTask(&channel, addCount, taskCount), CPUTask, time.Now())
 		}
 		<-channel
 	}
