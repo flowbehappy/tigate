5d95131b3f7ac6920689cecf5dee3b865f7c313f
dongmen <20351731+asddongmen@users.noreply.github.com>
2024-07-16 16:46:21 +0800

    eventservice: add eventservice basic test case (#63)
    Signed-off-by: dongmen <414110582@qq.com>

---
+++ pkg/eventservice/event_service_test.go
@@ -0,0 +1,201 @@
+package eventservice
+
+import (
+	"context"
+	"testing"
+	"time"
+
+	"github.com/flowbehappy/tigate/heartbeatpb"
+	"github.com/flowbehappy/tigate/pkg/common"
+	"github.com/flowbehappy/tigate/pkg/messaging"
+	"github.com/pingcap/log"
+	"github.com/stretchr/testify/require"
+	"go.uber.org/zap"
+)
+
+// mockMessageCenter is a mock implementation of the MessageCenter interface
+type mockMessageCenter struct {
+	messageCh chan *messaging.TargetMessage
+}
+
+func (m *mockMessageCenter) SendEvent(event ...*messaging.TargetMessage) error {
+	for _, e := range event {
+		m.messageCh <- e
+	}
+	return nil
+}
+
+func (m *mockMessageCenter) SendCommand(command ...*messaging.TargetMessage) error {
+	for _, c := range command {
+		m.messageCh <- c
+	}
+	return nil
+}
+
+func (m *mockMessageCenter) RegisterHandler(topic string, handler messaging.MessageHandler) {
+}
+
+func (m *mockMessageCenter) DeRegisterHandler(topic string) {
+}
+
+func (m *mockMessageCenter) AddTarget(id messaging.ServerId, epoch uint64, addr string) {
+}
+
+func (m *mockMessageCenter) RemoveTarget(id messaging.ServerId) {
+}
+
+func (m *mockMessageCenter) Close() {
+}
+
+// mockAcceptorInfo is a mock implementation of the AcceptorInfo interface
+type mockAcceptorInfo struct {
+	clusterID  uint64
+	serverID   string
+	id         string
+	topic      string
+	span       *common.TableSpan
+	startTs    uint64
+	isRegister bool
+}
+
+func newMockAcceptorInfo() *mockAcceptorInfo {
+	return &mockAcceptorInfo{
+		clusterID: 1,
+		serverID:  "server1",
+		id:        "id1",
+		topic:     "topic1",
+		span: &common.TableSpan{
+			&heartbeatpb.TableSpan{
+				TableID:  1,
+				StartKey: []byte("a"),
+				EndKey:   []byte("z"),
+			},
+		},
+		startTs:    1,
+		isRegister: true,
+	}
+}
+
+func (m *mockAcceptorInfo) GetID() string {
+	return m.id
+}
+
+func (m *mockAcceptorInfo) GetClusterID() uint64 {
+	return m.clusterID
+}
+
+func (m *mockAcceptorInfo) GetTopic() string {
+	return m.topic
+}
+
+func (m *mockAcceptorInfo) GetServerID() string {
+	return m.serverID
+}
+
+func (m *mockAcceptorInfo) GetTableSpan() *common.TableSpan {
+	return m.span
+}
+
+func (m *mockAcceptorInfo) GetStartTs() uint64 {
+	return m.startTs
+}
+
+func (m *mockAcceptorInfo) IsRegister() bool {
+	return m.isRegister
+}
+
+type mockSpanStats struct {
+	startTs       uint64
+	watermark     uint64
+	pendingEvents []*common.TxnEvent
+	onUpdate      func(watermark uint64)
+}
+
+func (m *mockSpanStats) update(event []*common.TxnEvent, watermark uint64) {
+	m.pendingEvents = append(m.pendingEvents, event...)
+	m.watermark = watermark
+	m.onUpdate(watermark)
+}
+
+// mockEventSource is a mock implementation of the EventSource interface
+type mockEventSource struct {
+	spans map[*common.TableSpan]*mockSpanStats
+}
+
+func newMockEventSource() *mockEventSource {
+	return &mockEventSource{
+		spans: make(map[*common.TableSpan]*mockSpanStats),
+	}
+}
+
+func (m *mockEventSource) SubscribeTableSpan(span *common.TableSpan, startTs uint64, onSpanUpdate func(watermark uint64)) (uint64, error) {
+	log.Info("subscribe table span", zap.Any("span", span), zap.Uint64("startTs", startTs))
+	m.spans[span] = &mockSpanStats{
+		startTs:       startTs,
+		watermark:     startTs,
+		onUpdate:      onSpanUpdate,
+		pendingEvents: make([]*common.TxnEvent, 0),
+	}
+	return startTs, nil
+}
+
+func (m *mockEventSource) Read(dataRange ...*common.DataRange) ([][]*common.TxnEvent, error) {
+	events := make([][]*common.TxnEvent, 0)
+	for _, dr := range dataRange {
+		events = append(events, m.spans[dr.Span].pendingEvents)
+		m.spans[dr.Span].pendingEvents = make([]*common.TxnEvent, 0)
+	}
+	return events, nil
+}
+
+func TestEventServiceBasic(t *testing.T) {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	eventSource := newMockEventSource()
+	mc := &mockMessageCenter{
+		messageCh: make(chan *messaging.TargetMessage, 100),
+	}
+
+	es := NewEventService(ctx, mc, eventSource)
+	esImpl := es.(*eventService)
+	go func() {
+		err := es.Run()
+		if err != nil {
+			t.Errorf("EventService.Run() error = %v", err)
+		}
+	}()
+
+	acceptorInfo := newMockAcceptorInfo()
+	// register acceptor
+	esImpl.acceptorInfoCh <- acceptorInfo
+	// wait for eventService to process the acceptorInfo
+	time.Sleep(time.Second * 2)
+
+	require.Equal(t, 1, len(esImpl.stores))
+	require.NotNil(t, esImpl.stores[acceptorInfo.GetClusterID()])
+	require.Equal(t, 1, len(esImpl.stores[acceptorInfo.GetClusterID()].spanStats))
+
+	// add events to eventSource
+	txnEvent := &common.TxnEvent{
+		ClusterID: 1,
+		Span:      acceptorInfo.span,
+		StartTs:   1,
+		CommitTs:  5,
+		Rows: []*common.RowChangedEvent{
+			{
+				PhysicalTableID: 1,
+			},
+		},
+	}
+
+	sourceSpanStat, ok := eventSource.spans[acceptorInfo.span]
+	require.True(t, ok)
+
+	sourceSpanStat.update([]*common.TxnEvent{txnEvent}, txnEvent.CommitTs)
+
+	// receive events from msg center
+	msg := <-mc.messageCh
+	require.NotNil(t, msg)
+	require.Equal(t, acceptorInfo.GetTopic(), msg.Topic)
+}
