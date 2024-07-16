d1445817846855660ede21804ba0846a55c0b4d4
Ling Jin <7138436+3AceShowHand@users.noreply.github.com>
2024-07-16 16:13:28 +0800

    fix some typo. (#62)

--- downstreamadapter/heartbeatcollector/heartbeat_collector.go
+++ downstreamadapter/heartbeatcollector/heartbeat_collector.go
@@ -45,15 +45,15 @@ type HeartBeatCollector struct {
 	eventDispatcherManagerMap   map[model.ChangeFeedID]*dispatchermanager.EventDispatcherManager // changefeedID -> EventDispatcherManager
 
 	responseChanMapMutex sync.RWMutex
-	reponseChanMap       map[model.ChangeFeedID]*dispatchermanager.HeartbeatResponseQueue //changefeedID -> HeartbeatResponseQueue
+	responseChanMap      map[model.ChangeFeedID]*dispatchermanager.HeartbeatResponseQueue //changefeedID -> HeartbeatResponseQueue
 	requestQueue         *dispatchermanager.HeartbeatRequestQueue
 }
 
 func NewHeartBeatCollector(serverId messaging.ServerId) *HeartBeatCollector {
 	heartBeatCollector := HeartBeatCollector{
-		from:           serverId,
-		requestQueue:   dispatchermanager.NewHeartbeatRequestQueue(),
-		reponseChanMap: make(map[model.ChangeFeedID]*dispatchermanager.HeartbeatResponseQueue),
+		from:            serverId,
+		requestQueue:    dispatchermanager.NewHeartbeatRequestQueue(),
+		responseChanMap: make(map[model.ChangeFeedID]*dispatchermanager.HeartbeatResponseQueue),
 	}
 	context.GetService[messaging.MessageCenter](context.MessageCenter).RegisterHandler(heartbeatResponseTopic, heartBeatCollector.RecvHeartBeatResponseMessages)
 	context.GetService[messaging.MessageCenter](context.MessageCenter).RegisterHandler(schedulerDispatcherTopic, heartBeatCollector.RecvSchedulerDispatcherRequestMessages)
@@ -73,7 +73,7 @@ func (c *HeartBeatCollector) RegisterEventDispatcherManager(m *dispatchermanager
 	defer c.eventDispatcherManagerMutex.Unlock()
 	defer c.responseChanMapMutex.Unlock()
 
-	c.reponseChanMap[m.ChangefeedID] = m.HeartbeatResponseQueue
+	c.responseChanMap[m.ChangefeedID] = m.HeartbeatResponseQueue
 	c.eventDispatcherManagerMap[m.ChangefeedID] = m
 
 	return nil
@@ -104,7 +104,7 @@ func (c *HeartBeatCollector) RecvHeartBeatResponseMessages(msg *messaging.Target
 
 	c.responseChanMapMutex.RLock()
 	defer c.responseChanMapMutex.RUnlock()
-	if queue, ok := c.reponseChanMap[changefeedID]; ok {
+	if queue, ok := c.responseChanMap[changefeedID]; ok {
 		queue.Enqueue(heartbeatResponse)
 	}
 	return nil
