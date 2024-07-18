9652ec7be2d7684b579263ebdfa06194d6cfee05
Jianyuan Jiang <sdojjy@qq.com>
2024-07-18 23:29:43 +0800

    add state_machine field to inferior (#90)

--- coordinator/coordinator.go
+++ coordinator/coordinator.go
@@ -193,7 +193,11 @@ func (c *coordinator) scheduleMaintainer(state *orchestrator.GlobalReactorState)
 		}
 		if shouldRunChangefeed(reactor.Info.State) {
 			// todo use real changefeed instance here
-			allChangefeedID.ReplaceOrInsert(scheduler.ChangefeedID(id), &changefeed{})
+			cf, ok := c.scheduledChangefeeds[id]
+			if !ok {
+				cf = &changefeed{}
+			}
+			allChangefeedID.ReplaceOrInsert(scheduler.ChangefeedID(id), cf)
 		}
 	}
 	tasks := c.scheduler.Schedule(
--- scheduler/state_machine.go
+++ scheduler/state_machine.go
@@ -124,6 +124,7 @@ func NewStateMachine(
 		Servers:  make(map[string]Role),
 		Inferior: inferior,
 	}
+	inferior.SetStateMachine(sm)
 	// Count of captures that is in Stopping states.
 	stoppingCount := 0
 	committed := false
