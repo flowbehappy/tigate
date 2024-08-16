175beefb353b2eaa14aab3c54d5b4a3522ef2d91
lidezhu <lidezhu@pingcap.com>
2024-08-16 10:04:40 +0800

    add logs

--- logservice/eventstore/eventstore.go
+++ logservice/eventstore/eventstore.go
@@ -20,7 +20,6 @@ import (
 	"github.com/pingcap/tidb/pkg/kv"
 	"github.com/pingcap/tiflow/pkg/pdutil"
 	"github.com/pingcap/tiflow/pkg/security"
-	"github.com/tikv/client-go/v2/oracle"
 	"github.com/tikv/client-go/v2/tikv"
 	pd "github.com/tikv/pd/client"
 	"go.uber.org/zap"
@@ -379,10 +378,11 @@ func (e *eventStore) writeEvent(span heartbeatpb.TableSpan, raw *common.RawKVEnt
 	}
 	if !raw.IsResolved() {
 		tableState.observer(raw)
-	} else {
-		log.Info("eventStore receive resolve ts",
-			zap.Any("time", oracle.GetTimeFromTS(raw.CRTs)))
 	}
+	// else {
+	// 	log.Info("eventStore receive resolve ts",
+	// 		zap.Any("time", oracle.GetTimeFromTS(raw.CRTs)))
+	// }
 	tableState.ch <- eventWithTableID{span: span, raw: raw}
 }
 
