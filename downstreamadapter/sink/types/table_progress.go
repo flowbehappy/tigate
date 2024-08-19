0e9c919cfdba69991394a6ffb2f86f8b187c2842
hongyunyan <649330952@qq.com>
2024-08-19 21:53:38 +0800

    remove conflict detector in sink logical

--- downstreamadapter/sink/types/table_progress.go
+++ downstreamadapter/sink/types/table_progress.go
@@ -20,9 +20,9 @@ import (
 	"github.com/flowbehappy/tigate/pkg/common"
 )
 
-// TableProgress 里面维护了目前正在 sink 中的 event ts 信息
+// TableProgress 里面维护了目前 sink 中的 event ts 信息
 // TableProgress 对外提供查询当前 table checkpointTs 的能力
-// TableProgress 对外提供当前 table 是否有 event 在 sink 中等待被 flush 的能力--用于判断 ddl 是否达到下推条件
+// TableProgress 对外提供当前 table 是否有 event 在等待被 flush 的能力--用于判断 ddl 是否达到下推条件
 //
 // 本质是要频繁的删除随机数据，插入递增数据，查询最小值，后面自己可以实现一个红黑树吧，或者其他结构，先用 list 苟一苟
 // 需要加个测试保证插入数据不会出现 commitTs 倒退的问题
@@ -56,6 +56,10 @@ func (p *TableProgress) Add(event *common.TxnEvent) {
 	elem := p.list.PushBack(ts)
 	p.elemMap[ts] = elem
 	p.maxCommitTs = event.CommitTs
+
+	event.PostTxnFlushed = func() {
+		p.Remove(event)
+	}
 }
 
 // 而且删除可以认为是批量的？但要不要做成批量可以后面再看
