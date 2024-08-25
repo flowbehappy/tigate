1db1f91599f246e4706f3c02f8c108e8b934c4fe
lidezhu <lidezhu@pingcap.com>
2024-08-25 13:30:19 +0800

    f

--- logservice/eventstore/eventstore.go
+++ logservice/eventstore/eventstore.go
@@ -119,7 +119,7 @@ func NewEventStore(
 		RegionRequestWorkerPerStore:        2,
 		ChangeEventProcessorNum:            32,
 		AdvanceResolvedTsIntervalInMs:      300,
-		RegionIncrementalScanLimitPerStore: 20,
+		RegionIncrementalScanLimitPerStore: 5,
 	}
 	client := logpuller.NewSubscriptionClient(
 		clientConfig,
