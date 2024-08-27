d88cc635504c80cdfaaf52d76cba9c86909607c8
hongyunyan <649330952@qq.com>
2024-08-27 21:48:01 +0800

    add comments

--- downstreamadapter/dispatcher/dispatcher.go
+++ downstreamadapter/dispatcher/dispatcher.go
@@ -200,7 +200,7 @@ func (d *Dispatcher) HandleEvent(event *common.TxnEvent) bool {
 	}
 }
 
-// 1.If the event is a single table DDL, it will be added to the sink for writing to downstream(async).
+// 1.If the event is a single table DDL, it will be added to the sink for writing to downstream(async). If the ddl leads to add new tables or drop tables, it should send heartbeat to maintainer
 // 2. If the event is a multi-table DDL, it will generate a TableSpanStatus message with ddl info to send to maintainer.
 func (d *Dispatcher) DealWithDDLWhenProgressEmpty() {
 	if d.ddlPendingEvent.IsSingleTableDDL() {
