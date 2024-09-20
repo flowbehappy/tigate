b24d1cd3b64b5d59e908984e8cc1d92217d72259
lidezhu <lidezhu@pingcap.com>
2024-09-20 19:26:59 +0800

    fix

--- logservice/schemastore/schema_store.go
+++ logservice/schemastore/schema_store.go
@@ -196,14 +196,6 @@ func (s *schemaStore) FetchTableDDLEvents(tableID int64, tableFilter filter.Filt
 	if currentResolvedTs < end {
 		end = currentResolvedTs
 	}
-	log.Debug("FetchTableDDLEvents",
-		zap.Int64("tableID", tableID),
-		zap.Uint64("start", start),
-		zap.Uint64("end", end))
-	defer log.Debug("FetchTableDDLEvents end",
-		zap.Int64("tableID", tableID),
-		zap.Uint64("start", start),
-		zap.Uint64("end", end))
 	events, err := s.dataStorage.fetchTableDDLEvents(tableID, tableFilter, start, end)
 	if err != nil {
 		return nil, 0, err
