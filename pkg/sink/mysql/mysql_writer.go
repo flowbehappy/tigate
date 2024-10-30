85cb850f385442a9d607eba17864a2a645ba87b1
hongyunyan <649330952@qq.com>
2024-10-30 22:39:45 +0800

    update

--- pkg/sink/mysql/mysql_writer.go
+++ pkg/sink/mysql/mysql_writer.go
@@ -375,8 +375,9 @@ func (w *MysqlWriter) SendDDLTs(event *commonEvent.DDLEvent) error {
 
 }
 
-// If no ddl-ts-v1 table, return 0; -- means the changefeed is new.
-// if no this row, return -1; -- means the table is dropped.(we won't check startTs for a table before it created)
+// If no ddl-ts-v1 table, return 0; -- means the downstream is new
+// If have ddl-ts-v1 table, but no row for the changefeed with tableID = 0, return 0; -- means the changefeed is new.
+// if have row for the changefeed with tableID = 0, but no this row, return -1; -- means the table is dropped.(we won't check startTs for a table before it created)
 // otherwise, return the ddl-ts value
 func (w *MysqlWriter) CheckStartTs(tableID int64, startTs uint64) (int64, error) {
 	ctx := context.Background()
@@ -432,7 +433,44 @@ func (w *MysqlWriter) CheckStartTs(tableID int64, startTs uint64) (int64, error)
 		return ddlTs, nil
 	} else {
 		// does't have this field
-		return -1, nil
+		// check whether have row for tableID = 0(table trigger event dispatcher)
+		// If changefeed has tables, it must have row for tableID = 0;
+
+		var builder strings.Builder
+		builder.WriteString("SELECT ddl_ts FROM ")
+		builder.WriteString(filter.TiCDCSystemSchema)
+		builder.WriteString(".")
+		builder.WriteString(filter.DDLTsTable)
+		builder.WriteString(" WHERE (ticdc_cluster_id, changefeed, table_id) IN (")
+
+		builder.WriteString("('")
+		builder.WriteString(ticdcClusterID)
+		builder.WriteString("', '")
+		builder.WriteString(changefeedID)
+		builder.WriteString("', ")
+		builder.WriteString(strconv.FormatInt(0, 10))
+		builder.WriteString(")")
+		builder.WriteString(")")
+		query := builder.String()
+
+		rows, err := tx.Query(query)
+		if err != nil {
+			log.Error("failed to check ddl ts table", zap.Error(err))
+			err2 := tx.Rollback()
+			if err2 != nil {
+				log.Error("failed to check ddl ts table", zap.Error(err2))
+			}
+			return 0, cerror.WrapError(cerror.ErrMySQLTxnError, errors.WithMessage(err, "failed to check ddl ts table;"))
+		}
+
+		defer rows.Close()
+		if rows.Next() {
+			// means has record for tableID = 0, but not for this table
+			return -1, nil
+		} else {
+			// no record for tableID = 0, means the changefeed is new.
+			return 0, nil
+		}
 	}
 }
 
