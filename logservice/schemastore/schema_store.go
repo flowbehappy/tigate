e26d4f553474cb297f576b1f394fbd47fa08ecec
lidezhu <lidezhu@pingcap.com>
2024-11-05 15:05:00 +0800

    add more log

--- logservice/schemastore/schema_store.go
+++ logservice/schemastore/schema_store.go
@@ -155,14 +155,14 @@ func (s *schemaStore) updateResolvedTsPeriodically(ctx context.Context) error {
 
 			for _, event := range resolvedEvents {
 				if event.Job.BinlogInfo.SchemaVersion <= s.schemaVersion || event.Job.BinlogInfo.FinishedTS <= s.finishedDDLTs {
-					// log.Info("skip already applied ddl job",
-					// 	zap.Any("type", event.Job.Type),
-					// 	zap.String("job", event.Job.Query),
-					// 	zap.Int64("jobSchemaVersion", event.Job.BinlogInfo.SchemaVersion),
-					// 	zap.Uint64("jobFinishTs", event.Job.BinlogInfo.FinishedTS),
-					// 	zap.Uint64("jobCommitTs", event.CommitTs),
-					// 	zap.Any("storeSchemaVersion", s.schemaVersion),
-					// 	zap.Uint64("storeFinishedDDLTS", s.finishedDDLTs))
+					log.Info("skip already applied ddl job",
+						zap.Any("type", event.Job.Type),
+						zap.String("job", event.Job.Query),
+						zap.Int64("jobSchemaVersion", event.Job.BinlogInfo.SchemaVersion),
+						zap.Uint64("jobFinishTs", event.Job.BinlogInfo.FinishedTS),
+						zap.Uint64("jobCommitTs", event.CommitTs),
+						zap.Any("storeSchemaVersion", s.schemaVersion),
+						zap.Uint64("storeFinishedDDLTS", s.finishedDDLTs))
 					continue
 				}
 				log.Info("handle ddl job",
