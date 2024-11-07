6a4bf5e080c57142dd6f55cefecdb4accd4141a1
lidezhu <47731263+lidezhu@users.noreply.github.com>
2024-11-07 15:45:29 +0800

    eventstore: fix wrong data sharing (#474)
    * eventstore: small fix
    
    * debug
    
    * add some log
    
    * add more log
    
    * fix

--- logservice/eventstore/event_store.go
+++ logservice/eventstore/event_store.go
@@ -350,9 +350,9 @@ func (e *eventStore) RegisterDispatcher(
 					log.Panic("should not happen")
 				}
 				// check whether startTs is in the range [checkpointTs, resolvedTs]
-				// for `[checkpointTs`: because we want data > startTs, so data <= startTs is deleted is ok.
+				// for `[checkpointTs`: because we want data > startTs, so data <= checkpointTs == startTs deleted is ok.
 				// for `resolvedTs]`: startTs == resolvedTs is a special case that no resolved ts has been recieved, so it is ok.
-				if subscriptionStat.checkpointTs <= startTs || startTs <= subscriptionStat.resolvedTs {
+				if subscriptionStat.checkpointTs <= startTs && startTs <= subscriptionStat.resolvedTs {
 					stat.subID = candidateDispatcher.subID
 					e.dispatcherStates.m[dispatcherID] = stat
 					// add dispatcher to existing subscription and return
@@ -366,7 +366,11 @@ func (e *eventStore) RegisterDispatcher(
 	}
 	e.dispatcherStates.Unlock()
 
-	// cannot share data from existing subscription, create a new one
+	// cannot share data from existing subscription, create a new subscription
+
+	// TODO: hash span is only needed when we need to reuse data after restart
+	// (if we finally decide not to reuse data after restart, use round robin instead)
+	// But if we need to share data for sub span, we need hash table id instead.
 	chIndex := common.HashTableSpan(tableSpan, len(e.eventChs))
 	uniqueKeyID := genUniqueID()
 	// Note: don't hold any lock when call Subscribe
@@ -456,6 +460,14 @@ func (e *eventStore) UpdateDispatcherSendTs(
 			if newCheckpointTs == 0 || dispatcherStat.checkpointTs < newCheckpointTs {
 				newCheckpointTs = dispatcherStat.checkpointTs
 			}
+		}
+		if newCheckpointTs == 0 {
+			return nil
+		}
+		if newCheckpointTs < subscriptionStat.checkpointTs {
+			log.Panic("should not happen",
+				zap.Uint64("newCheckpointTs", newCheckpointTs),
+				zap.Uint64("oldCheckpointTs", subscriptionStat.checkpointTs))
 		}
 		if subscriptionStat.checkpointTs < newCheckpointTs {
 			e.gcManager.addGCItem(
