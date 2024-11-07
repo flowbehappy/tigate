722a2c828124552aa3cf4830734fe1a4926cc3d7
dongmen <20351731+asddongmen@users.noreply.github.com>
2024-11-07 18:24:07 +0800

    metric, dispatcher: minor fix (#478)
    Signed-off-by: dongmen <414110582@qq.com>

--- downstreamadapter/dispatcher/helper.go
+++ downstreamadapter/dispatcher/helper.go
@@ -248,14 +248,25 @@ func (h *EventsHandler) Handle(dispatcher *Dispatcher, events ...DispatcherEvent
 	return dispatcher.HandleEvents(events)
 }
 
+const (
+	DataGroupResolvedTsOrDML = 1
+	DataGroupDDL             = 2
+	DataGroupSyncPoint       = 3
+	DataGroupHandshake       = 4
+)
+
 func (h *EventsHandler) GetType(event DispatcherEvent) dynstream.EventType {
 	switch event.GetType() {
 	case commonEvent.TypeResolvedEvent:
-		return dynstream.EventType{DataGroup: event.GetType(), Property: dynstream.PeriodicSignal}
+		return dynstream.EventType{DataGroup: DataGroupResolvedTsOrDML, Property: dynstream.PeriodicSignal}
 	case commonEvent.TypeDMLEvent:
-		return dynstream.EventType{DataGroup: event.GetType(), Property: dynstream.BatchableData}
-	case commonEvent.TypeDDLEvent, commonEvent.TypeSyncPointEvent, commonEvent.TypeHandshakeEvent:
-		return dynstream.EventType{DataGroup: event.GetType(), Property: dynstream.NonBatchable}
+		return dynstream.EventType{DataGroup: DataGroupResolvedTsOrDML, Property: dynstream.BatchableData}
+	case commonEvent.TypeDDLEvent:
+		return dynstream.EventType{DataGroup: DataGroupDDL, Property: dynstream.NonBatchable}
+	case commonEvent.TypeSyncPointEvent:
+		return dynstream.EventType{DataGroup: DataGroupSyncPoint, Property: dynstream.NonBatchable}
+	case commonEvent.TypeHandshakeEvent:
+		return dynstream.EventType{DataGroup: DataGroupHandshake, Property: dynstream.NonBatchable}
 	default:
 		log.Panic("unknown event type", zap.Int("type", int(event.GetType())))
 	}
