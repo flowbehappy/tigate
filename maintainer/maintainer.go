d3a6ca108b30b663e9cffd0e47051c080661eb21
Jianyuan Jiang <sdojjy@qq.com>
2024-11-08 10:46:31 +0800

    fix checkpoint stuck if no tables (#482)

--- maintainer/maintainer.go
+++ maintainer/maintainer.go
@@ -372,18 +372,17 @@ func (m *Maintainer) onNodeChanged() {
 	for id, _ := range currentNodes {
 		if _, ok := activeNodes[id]; !ok {
 			removedNodes = append(removedNodes, id)
+			delete(m.checkpointTsByCapture, id)
 			m.controller.RemoveNode(id)
 		}
 	}
-	log.Info("maintainer node changed",
-		zap.String("id", m.id.String()),
+	log.Info("maintainer node changed", zap.String("id", m.id.String()),
 		zap.Int("new", len(newNodes)),
 		zap.Int("removed", len(removedNodes)))
 	m.sendMessages(m.bootstrapper.HandleNewNodes(newNodes))
 	cachedResponse := m.bootstrapper.HandleRemoveNodes(removedNodes)
 	if cachedResponse != nil {
-		log.Info("bootstrap done after removed some nodes",
-			zap.String("id", m.id.String()))
+		log.Info("bootstrap done after removed some nodes", zap.String("id", m.id.String()))
 		m.onBootstrapDone(cachedResponse)
 	}
 }
@@ -405,16 +404,20 @@ func (m *Maintainer) calCheckpointTs() {
 	m.lastCheckpointTsTime = time.Now()
 
 	newWatermark := heartbeatpb.NewMaxWatermark()
+	// if there is no tables, there must be a table trigger dispatcher
 	for id, _ := range m.bootstrapper.GetAllNodes() {
-		if m.controller.GetTaskSizeByNodeID(id) > 0 {
-			if _, ok := m.checkpointTsByCapture[id]; !ok {
-				log.Debug("checkpointTs can not be advanced, since missing capture heartbeat",
-					zap.String("changefeed", m.id.Name()),
-					zap.Any("node", id))
-				return
-			}
-			newWatermark.UpdateMin(m.checkpointTsByCapture[id])
+		// maintainer node has the table trigger dispatcher
+		if id != m.selfNode.ID && m.controller.GetTaskSizeByNodeID(id) <= 0 {
+			continue
 		}
+		// node level watermark reported, ignore this round
+		if _, ok := m.checkpointTsByCapture[id]; !ok {
+			log.Debug("checkpointTs can not be advanced, since missing capture heartbeat",
+				zap.String("changefeed", m.id.Name()),
+				zap.Any("node", id))
+			return
+		}
+		newWatermark.UpdateMin(m.checkpointTsByCapture[id])
 	}
 	if newWatermark.CheckpointTs != math.MaxUint64 {
 		m.watermark.CheckpointTs = newWatermark.CheckpointTs
