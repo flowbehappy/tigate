5a27de2f460ff99a8045161d757c1defd7bdb953
hongyunyan <649330952@qq.com>
2024-11-08 14:41:28 +0800

    Fix dispatcher doesn't cancel the resend task for ddl events in high concurrency case(#487)
    * update
    
    * update

--- downstreamadapter/dispatcher/helper.go
+++ downstreamadapter/dispatcher/helper.go
@@ -26,6 +26,42 @@ import (
 	"go.uber.org/zap"
 )
 
+type ResendTaskMap struct {
+	mutex sync.Mutex
+	m     map[BlockEventIdentifier]*ResendTask
+}
+
+func newResendTaskMap() *ResendTaskMap {
+	return &ResendTaskMap{
+		m: make(map[BlockEventIdentifier]*ResendTask),
+	}
+}
+
+func (r *ResendTaskMap) Get(identifier BlockEventIdentifier) *ResendTask {
+	r.mutex.Lock()
+	defer r.mutex.Unlock()
+	return r.m[identifier]
+}
+
+func (r *ResendTaskMap) Set(identifier BlockEventIdentifier, task *ResendTask) {
+	r.mutex.Lock()
+	defer r.mutex.Unlock()
+	r.m[identifier] = task
+}
+
+func (r *ResendTaskMap) Delete(identifier BlockEventIdentifier) {
+	r.mutex.Lock()
+	defer r.mutex.Unlock()
+	delete(r.m, identifier)
+}
+
+// Considering the sync point event and ddl event may have the same commitTs,
+// we need to distinguish them.
+type BlockEventIdentifier struct {
+	CommitTs    uint64
+	IsSyncPoint bool
+}
+
 type BlockStatus struct {
 	mutex             sync.Mutex
 	blockPendingEvent commonEvent.BlockEvent
@@ -197,10 +233,6 @@ func (t *ResendTask) Execute() time.Time {
 }
 
 func (t *ResendTask) Cancel() {
-	_, blockStage := t.dispatcher.blockStatus.getEventAndStage()
-	if blockStage == heartbeatpb.BlockStage_NONE {
-		t.dispatcher.blockStatus.clear()
-	}
 	t.taskHandle.Cancel()
 }
 
