8ecdb0270bca76f5c3a80e83633f958221ea64a2
lidezhu <lidezhu@pingcap.com>
2024-11-23 16:11:00 +0800

    change config

--- logservice/eventstore/event_store.go
+++ logservice/eventstore/event_store.go
@@ -228,7 +228,7 @@ func New(
 
 	option := dynstream.NewOption()
 	option.InputBufferSize = 80000
-	option.BatchCount = 4096
+	option.BatchCount = 40960
 	ds := dynstream.NewParallelDynamicStream(streamCount, pathHasher{}, &eventsHandler{}, option)
 	ds.Start()
 
