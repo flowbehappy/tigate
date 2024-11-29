51b456a69c4ad6c622de0c4f4f22b1fca6175da1
dongmen <414110582@qq.com>
2024-11-29 10:58:59 +0800

    eventBroker: set message worker count to 1
    Signed-off-by: dongmen <414110582@qq.com>

--- pkg/eventservice/event_broker.go
+++ pkg/eventservice/event_broker.go
@@ -29,7 +29,7 @@ import (
 
 const (
 	resolvedTsCacheSize = 8192
-	streamCount         = 4
+	streamCount         = 2
 )
 
 var metricEventServiceSendEventDuration = metrics.EventServiceSendEventDuration.WithLabelValues("txn")
@@ -107,7 +107,7 @@ func newEventBroker(
 	ds := dynstream.NewParallelDynamicStream(streamCount, pathHasher{}, &dispatcherEventsHandler{}, option)
 	ds.Start()
 
-	messageWorkerCount := streamCount
+	messageWorkerCount := 1
 	//conf := config.GetGlobalServerConfig().Debug.EventService
 
 	c := &eventBroker{
