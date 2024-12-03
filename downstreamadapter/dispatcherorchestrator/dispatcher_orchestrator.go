bce683cb1ded35e083f270bd1442d7e2f8062ce3
hongyunyan <649330952@qq.com>
2024-12-03 11:40:09 +0800

    Fix panic for creating table trigger event dispatcher (#627)

--- downstreamadapter/dispatcherorchestrator/dispatcher_orchestrator.go
+++ downstreamadapter/dispatcherorchestrator/dispatcher_orchestrator.go
@@ -93,7 +93,7 @@ func (m *DispatcherOrchestrator) handleAddDispatcherManager(from node.ID, req *h
 		// check whether the event dispatcher manager has the table trigger event dispatcher
 		// when maintainer is transferred to a new node, maybe there has an event dispatcher manager without table trigger event dispatcher
 		// so we need to add a table trigger event dispatcher to the event dispatcher manager
-		if manager.GetTableTriggerEventDispatcher() == nil {
+		if manager.GetTableTriggerEventDispatcher() == nil && req.TableTriggerEventDispatcherId != nil {
 			startTs, err = manager.NewTableTriggerEventDispatcher(req.TableTriggerEventDispatcherId, req.StartTs)
 			if err != nil {
 				log.Error("failed to create new table trigger event dispatcher", zap.Error(err), zap.Any("ChangefeedID", cfId.Name()))
