b68b45ddbb661d3e344aef373476f6291aa2d4e1
dongmen <414110582@qq.com>
2024-12-13 16:12:05 +0800

    adjust sendMessageWorkerCount
    Signed-off-by: dongmen <414110582@qq.com>

--- pkg/eventservice/event_broker.go
+++ pkg/eventservice/event_broker.go
@@ -110,8 +110,8 @@ func newEventBroker(
 	// We noted that:
 	// 1. When the number of send message workers is too small, the lag of the resolvedTs keep in a high level.
 	// 2. When the number of send message workers is too large, the lag of the resolvedTs has spikes.
-	// And when the number of send message workers is 4 or 8, the lag of the resolvedTs is stable.
-	sendMessageWorkerCount := config.DefaultBasicEventHandlerConcurrency / 4
+	// And when the number of send message workers is 4, the lag of the resolvedTs is stable.
+	sendMessageWorkerCount := config.DefaultBasicEventHandlerConcurrency / 8
 	scanWorkerCount := config.DefaultBasicEventHandlerConcurrency * 4
 
 	conf := config.GetGlobalServerConfig().Debug.EventService
