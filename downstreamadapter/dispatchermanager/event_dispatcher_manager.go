bb5861ecdcbb7056fff89f4124c64b213f5b4867
hongyunyan <649330952@qq.com>
2024-12-20 10:37:41 +0800

    update

--- downstreamadapter/dispatchermanager/event_dispatcher_manager.go
+++ downstreamadapter/dispatchermanager/event_dispatcher_manager.go
@@ -267,6 +267,8 @@ func (e *EventDispatcherManager) close(removeChangefeed bool) {
 	metrics.CreateDispatcherDuration.DeleteLabelValues(e.changefeedID.Namespace(), e.changefeedID.Name())
 	metrics.EventDispatcherManagerCheckpointTsGauge.DeleteLabelValues(e.changefeedID.Namespace(), e.changefeedID.Name())
 	metrics.EventDispatcherManagerResolvedTsGauge.DeleteLabelValues(e.changefeedID.Namespace(), e.changefeedID.Name())
+	metrics.EventDispatcherManagerCheckpointTsLagGauge.DeleteLabelValues(e.changefeedID.Namespace(), e.changefeedID.Name())
+	metrics.EventDispatcherManagerResolvedTsLagGauge.DeleteLabelValues(e.changefeedID.Namespace(), e.changefeedID.Name())
 
 	e.closed.Store(true)
 	log.Info("event dispatcher manager closed", zap.Stringer("changefeedID", e.changefeedID))
